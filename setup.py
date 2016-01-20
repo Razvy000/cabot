04c838ae645da8209beec833bc49fb356bb665c4
Razvan Pistolea <razvy_000@gmai.com>
2016-01-20 20:35:20 +0000

    Add Plivo SMS Alert

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'wsgiref==0.1.2',
         'python-dateutil==2.1',
         'django-auth-ldap==1.2.6',
+        'plivo==0.11.0',
     ] + plugins,
     packages=find_packages(),
     include_package_data=True,
